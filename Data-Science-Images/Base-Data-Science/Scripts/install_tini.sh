226b3529877ff5336973f4ccbb221a73afbff795
Josh Cole <jwcole1@qinetiq.com>
2017-07-24 15:17:22 +0100

    update

--- Data-Science-Images/Base-Data-Science/Scripts/install_tini.sh
+++ Data-Science-Images/Base-Data-Science/Scripts/install_tini.sh
@@ -1,5 +1,9 @@
+mkdir -p $TINI_DIR
+cd $TINI_DIR
+wget --quiet https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini
+wget --quiet https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini.asc
 gpg --keyserver ha.pool.sks-keyservers.net --recv-keys 595E85A6B1B4779EA4DAAEC70B588DFF0527A9B7 \
- && gpg --verify /opt/tini/tini.asc
-chmod +x /opt/tini/tini
-chown -R $DATASCI_USER:$DATASCI_USER /opt/tini
-cp /opt/tini/tini /usr/bin/tini
+ && gpg --verify $TINI_DIR/tini.asc
+chmod +x $TINI_DIR/tini
+chown -R $DATASCI_USER:$DATASCI_USER $TINI_DIR
+cp $TINI_DIR/tini /usr/bin/tini
