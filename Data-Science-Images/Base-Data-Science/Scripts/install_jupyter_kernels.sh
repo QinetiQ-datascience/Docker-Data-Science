305ad67e7f8dea1b89a42da0fbfe0a41b79296f4
Josh Cole <jwcole1@qinetiq.com>
2017-07-24 13:32:40 +0100

    update

--- Data-Science-Images/Base-Data-Science/Scripts/install_jupyter_kernels.sh
+++ Data-Science-Images/Base-Data-Science/Scripts/install_jupyter_kernels.sh
@@ -4,6 +4,6 @@ $CONDA_BIN/jupyter kernelspec install $SAGE_ROOT/local/share/jupyter/kernels/sag
 /usr/lib/sagemath/local/bin/python2.7 -m pip install ipykernel
 /usr/lib/sagemath/local/bin/python2.7 -m ipykernel install --user
 $CONDA_BIN/pip install https://dist.apache.org/repos/dist/dev/incubator/toree/${TORRE_VERSION}/snapshots/dev1/toree-pip/toree-${TORRE_VERSION}.dev1.tar.gz
-$CONDA_BIN/jupyter toree install --user --spark_home=$SPARK_HOME --interpreters=PySpark,SQL,Scala,R
-$CONDA_BIN/conda install -c conda-forge spylon-kernel && $CONDA_BIN/python -m spylon_kernel install
+$CONDA_BIN/jupyter toree install --user --spark_home=$SPARK_HOME --interpreters=PySpark,SQL,Scala,SparkR
+$CONDA_BIN/conda install --yes -c conda-forge spylon-kernel && $CONDA_BIN/python -m spylon_kernel install
 $CONDA_BIN/pip install bash_kernel && $CONDA_BIN/python -m bash_kernel.install
