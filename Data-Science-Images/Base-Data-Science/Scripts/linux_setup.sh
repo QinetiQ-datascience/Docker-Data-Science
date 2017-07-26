a5e86daf6a76a95f3d27a4cea0717a3f417dd7db
Josh Cole <jwcole1@qinetiq.com>
2017-07-26 18:12:35 +0100

    cleanup

--- Data-Science-Images/Base-Data-Science/Scripts/linux_setup.sh
+++ Data-Science-Images/Base-Data-Science/Scripts/linux_setup.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Dev Tools
-apt --yes update && apt --yes install software-properties-common python-software-properties build-essential automake
-apt --yes update && apt --yes install  wget curl unzip zip bzip2 fonts-liberation python-dev libjpeg8-dev zlib1g-dev gfortran  fonts-dejavu ca-certificates \
+apt update && apt --yes install software-properties-common python-software-properties build-essential automake
+apt update && apt --yes install  wget curl unzip zip bzip2 fonts-liberation python-dev libjpeg8-dev zlib1g-dev gfortran  fonts-dejavu ca-certificates \
 libcairo2 libpango1.0-0 libpcre3-dev gettext hdf5-tools m4 libssl-dev libcurl4-openssl-dev libzmq3-dev cmake unzip libsm6 jed emacs libxrender1 \
 libtinfo-dev libcairo2-dev libpango1.0-dev libmagic-dev libblas-dev liblapack-dev --no-install-recommends libzmq3-dev libgmp-dev libffi-dev libunwind-dev \
 ghostscript gnuplot graphviz less xvfb libxss1 libgconf-2-4 libnss3 libasound2 net-tools
@@ -11,19 +11,19 @@ ghostscript gnuplot graphviz less xvfb libxss1 libgconf-2-4 libnss3 libasound2 n
 # Install Java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \
 add-apt-repository --yes ppa:webupd8team/java && \
-apt --yes update && \
+apt update && \
 apt install --yes  oracle-java8-installer oracle-java8-set-default && \
 rm -rf /var/lib/apt/lists/* && \
 rm -rf /var/cache/oracle-jdk8-installer
 
 # Install Nodejs
-apt --yes update && apt install --yes nodejs-legacy npm
+apt update && apt install --yes nodejs-legacy npm
 
 # Version control
-apt --yes update && apt install --yes git subversion mercurial --no-install-recommends
+apt update && apt install --yes git subversion mercurial --no-install-recommends
 
 # Documentation generation requirements for jupyter
-apt --yes update && apt --yes install texlive texlive-latex-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended texlive-generic-recommended pandoc --no-install-recommends
+apt update && apt --yes install texlive texlive-latex-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended texlive-generic-recommended pandoc --no-install-recommends
 
 # Install Boost
-apt --yes update && add-apt-repository universe && apt --yes update && apt --yes install  libboost-all-dev
+apt update && add-apt-repository universe && apt update && apt --yes install  libboost-all-dev
