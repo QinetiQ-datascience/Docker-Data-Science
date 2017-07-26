98dd5ed88f2ee13ac3ca695c8f889267ae74c55a
Josh Cole <jwcole1@qinetiq.com>
2017-07-26 18:06:20 +0100

    cleanup

--- Data-Science-Images/Base-Data-Science/Scripts/linux_setup.sh
+++ Data-Science-Images/Base-Data-Science/Scripts/linux_setup.sh
@@ -6,7 +6,7 @@ apt --yes update && apt --yes install software-properties-common python-software
 apt --yes update && apt --yes install  wget curl unzip zip bzip2 fonts-liberation python-dev libjpeg8-dev zlib1g-dev gfortran  fonts-dejavu ca-certificates \
 libcairo2 libpango1.0-0 libpcre3-dev gettext hdf5-tools m4 libssl-dev libcurl4-openssl-dev libzmq3-dev cmake unzip libsm6 jed emacs libxrender1 \
 libtinfo-dev libcairo2-dev libpango1.0-dev libmagic-dev libblas-dev liblapack-dev --no-install-recommends libzmq3-dev libgmp-dev libffi-dev libunwind-dev \
-ghostscript ttf-opensans fltk gnuplot graphviz lapack less xvfb libxss1 libgconf-2-4 libnss3 libasound2 net-tools
+ghostscript gnuplot graphviz less xvfb libxss1 libgconf-2-4 libnss3 libasound2 net-tools
 
 # Install Java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \
