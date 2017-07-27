db418ca16d4a1a930e16dcf75f63850402b37ee7
Josh Cole <joshcole@joshs-mbp.home>
2017-07-28 00:16:28 +0100

    update

--- Ubuntu/Data-Science-Images/Base-Data-Science/Scripts/languages_setup.sh
+++ Ubuntu/Data-Science-Images/Base-Data-Science/Scripts/languages_setup.sh
@@ -20,7 +20,7 @@ apt-get update && apt-get --yes install scala
 echo "deb http://ppa.launchpad.net/staticfloat/juliareleases/ubuntu trusty main" > /etc/apt/sources.list.d/julia.list && \
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3D3D3ACC && \
 apt-get update && apt-get install -y --no-install-recommends \
-julia && apt clean && \
+julia && apt-get clean && \
 rm -rf /var/lib/apt/lists/* && \
 
 echo "push!(Libdl.DL_LOAD_PATH, \"$CONDA_DIR/lib\")" >> /usr/etc/julia/juliarc.jl && \
