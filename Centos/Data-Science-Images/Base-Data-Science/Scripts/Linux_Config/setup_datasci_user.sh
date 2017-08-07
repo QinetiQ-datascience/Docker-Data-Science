397be1f31aa16d0d1a43d3dd0dd9be4049bb2a97
Josh Cole <jwcole1@qinetiq.com>
2017-08-07 09:39:37 +0100

    centos update

--- Centos/Data-Science-Images/Base-Data-Science/Scripts/Linux_Config/setup_datasci_user.sh
+++ Centos/Data-Science-Images/Base-Data-Science/Scripts/Linux_Config/setup_datasci_user.sh
@@ -9,8 +9,13 @@ mkdir -p $CONDA_DIR && chown $DATASCI_USER $CONDA_DIR
 chown -R $DATASCI_USER:$DATASCI_USER $CONDA_DIR
 
 # Users passwords
-echo "$DATASCI_USER:$DATASCI_USER"|chpasswd  && gpasswd -a $DATASCI_USER wheel 
-echo "root:Docker!" | chpasswd
+# echo "$DATASCI_USER:$DATASCI_USER"|chpasswd  && gpasswd -a $DATASCI_USER wheel 
+# echo "root:Docker!" | chpasswd
+
+echo "$DATASCI_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
+echo "root:$DATASCI_USER" | chpasswd && \
+su $DATASCI_USER echo "$DATASCI_USER:$DATASCI_USER" | chpasswd  && gpasswd -a $DATASCI_USER wheel
+
 mkdir -p $CONDA_SRC && chown -R $DATASCI_USER:$DATASCI_USER $CONDA_SRC
 mkdir -p $Documents && chown -R $DATASCI_USER:$DATASCI_USER $Documents
 mkdir -p $Downloads && chown -R $DATASCI_USER:$DATASCI_USER $Downloads
