c4d2e82f164b5e5abcb4600ff5d0fe3c76079468
Josh Cole <jwcole1@qinetiq.com>
2017-08-10 17:37:40 +0100

    Rstudio server, refactor and update

--- Scripts/Linux/setup_datasci_user.sh
+++ Scripts/Linux/setup_datasci_user.sh
@@ -147,9 +147,9 @@ cd /tmp && wget https://storage.googleapis.com/golang/go$GOLANG_VERSION.linux-am
 mkdir -p $TINI_DIR
 cd $TINI_DIR
 wget --quiet https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini
-wget --quiet https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini.asc
-gpg --keyserver ha.pool.sks-keyservers.net --recv-keys $TINI_CHECKSUM
-gpg --verify $TINI_DIR/tini.asc
+# wget --quiet https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini.asc
+# gpg --keyserver ha.pool.sks-keyservers.net --recv-keys $TINI_CHECKSUM
+# gpg --verify $TINI_DIR/tini.asc
 chmod +x $TINI_DIR/tini
 chown -R $DATASCI_USER:$DATASCI_USER $TINI_DIR
 cp $TINI_DIR/tini /usr/bin/tini
