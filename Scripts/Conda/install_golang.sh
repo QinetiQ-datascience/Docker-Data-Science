e755253a1e66314998cb70ce5a641eaaaf2243da
Josh Cole <jwcole1@qinetiq.com>
2017-08-25 12:22:28 +0100

    update

--- Scripts/Conda/install_golang.sh
+++ Scripts/Conda/install_golang.sh
@@ -4,6 +4,6 @@ set -e
 cd /tmp && wget https://storage.googleapis.com/golang/go$GOLANG_VERSION.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz
 # TODO Fix Checksum
 
-go get golang.org/x/tools/cmd/goimports
+# go get golang.org/x/tools/cmd/goimports
 
-go get -tags zmq_4_x github.com/gopherds/gophernotes
+# go get -tags zmq_4_x github.com/gopherds/gophernotes
